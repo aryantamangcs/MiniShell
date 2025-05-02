8dbca8884fa86e8ec4488acb2d86d695f0e24ce5
aryantmg628 <aryantmg628@gmail.com>
2025-05-02 10:04:43 +0545

    Feat : parser added

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,18 @@
 #include "../include/parser.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_ARGS 20
 
-void parse_command(char *input, char **args, int *background) {
-  printf("Parsing the commands");
+char **parse_command(char *input, int *background) {
+  char **tokens = malloc(MAX_ARGS * sizeof(char *));
+  int token_count = 0;
+  char *token = strtok(input, " ");
+
+  while (token != NULL && token_count < MAX_ARGS) {
+    tokens[token_count++] = token;
+    token = strtok(NULL, " ");
+  }
+
+  return tokens;
 }
