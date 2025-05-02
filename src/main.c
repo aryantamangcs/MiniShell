72deb40181898562712a21621921fb5cf42ab485
aryantmg628 <aryantmg628@gmail.com>
2025-05-02 10:03:43 +0545

    Feat : command parsing and execution added

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include "../include/execute_commands.h"
+#include "../include/parser.h"
 #include <stdio.h>
 #include <string.h>
 #define MAX_INPUT_SIZE 4096
@@ -6,6 +8,8 @@
 
 int main() {
   char input[MAX_INPUT_SIZE];
+  char *args;
+  int background = 0;
 
   while (True) {
     printf("mini-shell> ");
@@ -14,11 +18,17 @@ int main() {
       perror("fgets failed");
       continue;
     }
+    // replacing the \n with \0
+    input[strcspn(input, "\n")] = '\0';
 
     if (strncmp(input, "exit", 4) == 0) {
       printf("Exiting mini-shell......\n");
       break;
     }
+
+    char **tokens = parse_command(input, &background);
+
+    execute_command(tokens);
   }
   return 0;
 }
