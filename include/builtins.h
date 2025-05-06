d836f6bf185b5ae8ada205dfa82cca454c008e96
aryantmg628 <aryantmg628@gmail.com>
2025-05-06 10:15:49 +0545

    refactor : refactor the code

--- include/builtins.h
+++ include/builtins.h
@@ -3,8 +3,10 @@
 #define True 1
 #define False 0
 
+extern char *get_prompt();
 extern void custom_ls(int argc, char *args[]);
 extern void custom_cat(int argc, char *args[]);
+extern void custom_cd(int argc, char *args[]);
 extern char *custom_pwd();
 extern void custom_clear();
 
